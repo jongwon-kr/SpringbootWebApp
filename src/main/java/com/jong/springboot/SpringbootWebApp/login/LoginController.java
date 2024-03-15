4c6993d3773b5705cc05e817792953adfbdd4636
jongwon-kr <growjong8802@gmail.com>
2024-03-15 18:32:34 +0900

    private Logger logger = LoggerFactory.getLogger(getClass());
    로그 표시 클래스 log = String등등
    logger.debug(log), logger.info(log)등등 으로 표시하고 싶은 수준을 선택하여 로깅할 수 있다.

--- src/main/java/com/jong/springboot/SpringbootWebApp/login/LoginController.java
+++ src/main/java/com/jong/springboot/SpringbootWebApp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.jong.springboot.SpringbootWebApp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +9,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-
+    private Logger logger = LoggerFactory.getLogger(getClass());
     ///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
 
     // http://localhost:8080/login?name=jong
     @RequestMapping("login")
     public String goToLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        System.out.println("Request parm name = " + name);
-
+        logger.debug("(debug)Request Parm is {}",name);
+        logger.info("(info)Request Parm is {}",name);
+        logger.info("(warn)Request Parm is {}",name);
+        System.out.println("Request Parm is " + name);
         return "login";
     }
 }
