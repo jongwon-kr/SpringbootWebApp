1c2307e83e4fddedf2a4e813c7e1ac1ec69cda10
jongwon-kr <growjong8802@gmail.com>
2024-03-16 21:54:54 +0900

    reset LoginController

--- src/main/java/com/jong/springboot/SpringbootWebApp/login/LoginController.java
+++ src/main/java/com/jong/springboot/SpringbootWebApp/login/LoginController.java
@@ -9,17 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-    ///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
-
-    // http://localhost:8080/login?name=jong
+    // login
     @RequestMapping("login")
-    public String goToLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
-        logger.debug("(debug)Request Parm is {}",name);
-        logger.info("(info)Request Parm is {}",name);
-        logger.info("(warn)Request Parm is {}",name);
-        System.out.println("Request Parm is " + name);
+    public String goToLoginPage() {
         return "login";
     }
 }
