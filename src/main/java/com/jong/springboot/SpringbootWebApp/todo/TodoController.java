8cfe54c4cc0e8ea64ea4d5e3113ba119f67ab2b9
jongwon-kr <growjong8802@gmail.com>
2024-03-16 22:39:06 +0900

    @SessionAttributes("name")
    세션 name에 값을 다른 컨트롤러와 공유 가능 값을 부여받는 컨트롤러와 그 값을 사용하고 싶은 컨트롤러 모두 @SessionAttributes이 필요함

---
+++ src/main/java/com/jong/springboot/SpringbootWebApp/todo/TodoController.java
@@ -0,0 +1,31 @@
+package com.jong.springboot.SpringbootWebApp.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.List;
+
+@Controller
+@SessionAttributes("name")
+public class TodoController {
+
+    private TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    // /list-todos
+    @RequestMapping("list-todos")
+    public String listAllTodos(ModelMap model) {
+        List<Todo> todos = todoService.findByUsername("jongwon");
+        model.addAttribute("todos", todos);
+
+        return "listTodos";
+    }
+
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Welcome Page</title>
+	</head>
+	<body>
+		<div>Welcome to in28minutes</div>
+		<div>Your Name: ${name}</div>
+		<a href="list-todos">Manage</a> your todos
+	</body>
+</html>
